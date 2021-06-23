c06599c5d7fab25de1271ad3a01358f135b8053e
Scott Seago <sseago@redhat.com>
2021-06-23 11:45:44 -0400

    initial implementation of transform command (#8)

---
+++ internal/plugin/plugin_helper.go
@@ -0,0 +1,45 @@
+package plugin
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/binary-plugin"
+)
+func GetBinaryPlugins(dir string) ([]transform.Plugin, error) {
+	files, err := ioutil.ReadDir(dir)
+	switch {
+	case os.IsNotExist(err):
+		return []transform.Plugin{}, nil
+	case err != nil:
+		return nil, err
+	}
+	return getBinaryPlugins(dir, files)
+}
+
+func getBinaryPlugins(path string, files []os.FileInfo) ([]transform.Plugin, error) {
+	pluginList := []transform.Plugin{}
+	for _, file := range files {
+		filePath := fmt.Sprintf("%v/%v", path, file.Name())
+		if file.IsDir() {
+			newFiles, err := ioutil.ReadDir(filePath)
+			if err != nil {
+				return nil, err
+			}
+			plugins, err := getBinaryPlugins(filePath, newFiles)
+			if err != nil {
+				return nil, err
+			}
+			pluginList = append(pluginList, plugins...)
+		} else if file.Mode().IsRegular() && isExecAny(file.Mode().Perm()) {
+			pluginList = append(pluginList, binary_plugin.NewBinaryPlugin(filePath))
+		}
+	}
+	return pluginList, nil
+}
+
+func isExecAny(mode os.FileMode) bool {
+    return mode&0111 != 0
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/konveyor/crane/cmd/apply"
 	export "github.com/konveyor/crane/cmd/export"
+	"github.com/konveyor/crane/cmd/transform"
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
@@ -14,6 +15,7 @@ func main() {
 		Use: "crane",
 	}
 	root.AddCommand(export.NewExportCommand(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}))
+	root.AddCommand(transform.NewTransformCommand())
 	root.AddCommand(apply.NewApplyCommand())
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
