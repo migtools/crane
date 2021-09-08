d70d39d7624b99e7f20ae232acc2a9ecffb890ca
Jason Montleon <jmontleo@redhat.com>
2021-09-08 09:58:56 -0400

    add api vpn access

---
+++ cmd/tunnel-api/tunnel-api.go
@@ -0,0 +1,170 @@
+package tunnel_api
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/konveyor/crane-lib/connect/tunnel_api"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type TunnelAPIOptions struct {
+	configFlags *genericclioptions.ConfigFlags
+	genericclioptions.IOStreams
+
+	logger             logrus.FieldLogger
+	SourceContext      string
+	DestinationContext string
+	Namespace          string
+
+	sourceContext      *clientcmdapi.Context
+	destinationContext *clientcmdapi.Context
+}
+
+func NewTunnelAPIOptions(streams genericclioptions.IOStreams) *cobra.Command {
+	t := &TunnelAPIOptions{
+		configFlags: genericclioptions.NewConfigFlags(false),
+
+		IOStreams: streams,
+		logger:    logrus.New(),
+	}
+
+	cmd := &cobra.Command{
+		Use:   "tunnel-api",
+		Short: "set up an openvpn tunnel to access an (source) on premise cluster from a (cloud) destination cluster",
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := t.Complete(c, args); err != nil {
+				return err
+			}
+			if err := t.Validate(); err != nil {
+				return err
+			}
+			if err := t.Run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+	addFlagsForTunnelAPIOptions(t, cmd)
+
+	return cmd
+}
+
+func addFlagsForTunnelAPIOptions(t *TunnelAPIOptions, cmd *cobra.Command) {
+	cmd.Flags().StringVar(&t.SourceContext, "source-context", "", "The name of the source context in current kubeconfig")
+	cmd.Flags().StringVar(&t.DestinationContext, "destination-context", "", "The name of destination context current kubeconfig")
+	cmd.Flags().StringVar(&t.Namespace, "namespace", "", "The namespace of the pvc which is to be transferred, if empty it will try to use the openvpn namespace")
+}
+
+func (t *TunnelAPIOptions) Complete(c *cobra.Command, args []string) error {
+	config := t.configFlags.ToRawKubeConfigLoader()
+	rawConfig, err := config.RawConfig()
+	if err != nil {
+		return err
+	}
+
+	if t.DestinationContext == "" {
+		t.DestinationContext = *t.configFlags.Context
+	}
+
+	for name, context := range rawConfig.Contexts {
+		if name == t.SourceContext {
+			t.sourceContext = context
+		}
+		if name == t.DestinationContext {
+			t.destinationContext = context
+		}
+	}
+
+	return nil
+}
+
+func (t *TunnelAPIOptions) Validate() error {
+	if t.sourceContext == nil {
+		return fmt.Errorf("cannot evaluate source context")
+	}
+
+	if t.destinationContext == nil {
+		return fmt.Errorf("cannot evaluate destination context")
+	}
+
+	if t.sourceContext.Cluster == t.destinationContext.Cluster {
+		return fmt.Errorf("both source and destination cluster are same, this is not supported")
+	}
+
+	return nil
+}
+
+func (t *TunnelAPIOptions) Run() error {
+	return t.run()
+}
+
+func (t *TunnelAPIOptions) getClientFromContext(ctx string) (client.Client, error) {
+	restConfig, err := t.getRestConfigFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.New(restConfig, client.Options{Scheme: scheme.Scheme})
+}
+
+func (t *TunnelAPIOptions) getRestConfigFromContext(ctx string) (*rest.Config, error) {
+	c := ctx
+	t.configFlags.Context = &c
+
+	return t.configFlags.ToRESTConfig()
+}
+
+func (t *TunnelAPIOptions) run() error {
+	tunnel := tunnel_api.Tunnel{}
+
+	fmt.Println("Generating SSL certificates. This may take several minutes.")
+	ca, serverCrt, serverKey, clientCrt, clientKey, dh, err := tunnel_api.GenOpenvpnSSLCrts()
+	if err != nil {
+		return err
+	}
+	tunnel.Options.CACrt = ca
+	tunnel.Options.ServerCrt = serverCrt
+	tunnel.Options.ServerKey = serverKey
+	tunnel.Options.ClientCrt = clientCrt
+	tunnel.Options.ClientKey = clientKey
+	tunnel.Options.RSADHKey = dh
+	fmt.Println("SSL Certificate generation complete.")
+
+	srcConfig, err := t.getRestConfigFromContext(t.SourceContext)
+	if err != nil {
+		log.Fatal(err, "unable to get source config")
+	}
+
+	dstConfig, err := t.getRestConfigFromContext(t.DestinationContext)
+	if err != nil {
+		log.Fatal(err, "unable to get destination config")
+	}
+
+	_, err = t.getClientFromContext(t.SourceContext)
+	if err != nil {
+		log.Fatal(err, "unable to get source client")
+	}
+	_, err = t.getClientFromContext(t.DestinationContext)
+	if err != nil {
+		log.Fatal(err, "unable to get destination client")
+	}
+
+	tunnel.SrcConfig = srcConfig
+	tunnel.DstConfig = dstConfig
+	tunnel.Options.Namespace = t.Namespace
+
+	err = tunnel_api.Openvpn(tunnel)
+	if err != nil {
+		log.Fatal(err, "Unable to create Tunnel")
+	}
+
+	return nil
+}
