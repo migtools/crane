e0c3be9fde8c6120e44eb31dddb3cd5ca0377828
Scott Seago <sseago@redhat.com>
2021-11-15 12:03:21 -0500

    add version command; update crane-lib to 0.0.4 (#52)

---
+++ internal/buildinfo/buildinfo.go
@@ -0,0 +1,10 @@
+package buildinfo
+
+import (
+	cranelibversion "github.com/konveyor/crane-lib/version"
+)
+var (
+	Version string = "v0.0.2"
+
+	CranelibVersion string = cranelibversion.Version
+)
