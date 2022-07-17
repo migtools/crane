f3024fc37dd2a912ded9ae2dd87c4b2978c55cc3
Ankur Mundra <amundra@redhat.com>
2022-07-17 13:42:51 -0400

    modified command help message

--- cmd/runfn/runfn.go
+++ cmd/runfn/runfn.go
@@ -36,7 +36,8 @@ func NewFnRunCommand(f *flags.GlobalFlags) *cobra.Command {
 	}
 	cmd := &cobra.Command{
 		Use:          "runfn [IMAGE] [flags] [--args]",
-		Short:        "Transform resources by executing function \nExperimental: This command is under active development and may change without notice.",
+		Long:         "Transform resources by executing KRM function \n\nExperimental: This command is under active development and may change without notice.",
+		Short:        "Transform resources by executing KRM function",
 		RunE:         o.runE,
 		PreRunE:      o.preRunE,
 		SilenceUsage: true,
