74262d423ecd4268514d807f1260a967412e3e86
M Sajid Mansoori <87861083+msajidmansoori12@users.noreply.github.com>
2026-05-29 15:28:30 +0530

    tier1: automate suggestion for alternative gv (#421)
    * Add validate command to framework
    
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>
    
    * Add validatedir to scenario paths
    
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>
    
    * Automate validate alternative GV suggestion test
    
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>
    
    * Re
    
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>
    
    ---------
    
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>

--- e2e-tests/framework/crane.go
+++ e2e-tests/framework/crane.go
@@ -22,6 +22,16 @@ type TransferPVCOptions struct {
 	Subdomain       string
 }
 
+// ValidateOptions contains arguments for the crane validate command.
+type ValidateOptions struct {
+	Context          string
+	InputDir         string
+	ValidateDir      string
+	OutputFormat     string
+	APIResourcesFile string
+	ExtraArgs        []string
+}
+
 // Export runs crane export for a namespace into the given export directory.
 func (c CraneRunner) Export(namespace, exportDir string) error {
 	args := []string{"export", "--context", c.SourceContext, "--namespace", namespace, "--export-dir", exportDir}
@@ -107,4 +117,38 @@ func (c CraneRunner) TransferPVC(opts TransferPVCOptions) error {
 		return fmt.Errorf("crane transfer-pvc failed: %v, output: %s", err, string(out))
 	}
 	return nil
+}
+
+// Validate runs crane validate and returns command output or an error.
+func (c CraneRunner) Validate(opts ValidateOptions) (stdout string, err error) {
+	args := []string{"validate"}
+
+	if opts.Context != "" {
+		args = append(args, "--context", opts.Context)
+	}
+	if opts.InputDir != "" {
+		args = append(args, "--input-dir", opts.InputDir)
+	}
+	if opts.ValidateDir != "" {
+		args = append(args, "--validate-dir", opts.ValidateDir)
+	}
+	if opts.OutputFormat != "" {
+		args = append(args, "--output", opts.OutputFormat)
+	}
+	if opts.APIResourcesFile != "" {
+		args = append(args, "--api-resources", opts.APIResourcesFile)
+	}
+	if len(opts.ExtraArgs) > 0 {
+		args = append(args, opts.ExtraArgs...)
+	}
+	logVerboseCommand(c.Bin, args)
+	cmd := exec.Command(c.Bin, args...)
+	cmd.Dir = c.WorkDir
+	out, cmdErr := cmd.CombinedOutput()
+	logVerboseOutput("crane validate", out)
+	stdout = string(out)
+	if cmdErr != nil {
+		return stdout, fmt.Errorf("crane validate failed: %v, output: %s", cmdErr, stdout)
+	}
+	return stdout, nil
 }
