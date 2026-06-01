4ada3dc9c1f2dff2cc7cff2f4fc3def9ba076bfb
Sachin Singla <ssingla@redhat.com>
2026-06-01 07:26:54 +0530

    Added kustomize version (#413)
    * Added kustomize version
    
    * fix coderabbit comments
    
    * Fix kustomize CLI version

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -1,11 +1,33 @@
 package buildinfo
 
 import (
+	"runtime/debug"
+
 	cranelibversion "github.com/konveyor/crane-lib/version"
+	// Blank import to keep the kustomize CLI module in the dependency graph
+	// so debug.ReadBuildInfo() can report its version.
+	_ "sigs.k8s.io/kustomize/kustomize/v5/commands/version"
 )
 
 var (
 	Version string = "v0.0.6"
 
 	CranelibVersion string = cranelibversion.Version
+
+	KustomizeVersion string = readKustomizeVersion()
 )
+
+var readBuildInfo = debug.ReadBuildInfo
+
+func readKustomizeVersion() string {
+	info, ok := readBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+	for _, dep := range info.Deps {
+		if dep.Path == "sigs.k8s.io/kustomize/kustomize/v5" {
+			return dep.Version
+		}
+	}
+	return "unknown"
+}
