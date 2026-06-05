88d3d11abddac8829ff623167933082aa31ae7bf
M Sajid Mansoori <mmansoor@redhat.com>
2026-06-05 12:12:59 +0530

    Add git sha as part of crane version command
    Signed-off-by: M Sajid Mansoori <mmansoor@redhat.com>

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -10,9 +10,12 @@ import (
 )
 
 var (
+	// Version and BuildCommit can be overridden at build time with:
+	// go build -ldflags="-X github.com/konveyor/crane/internal/buildinfo.Version=<version> -X github.com/konveyor/crane/internal/buildinfo.BuildCommit=$(git rev-parse HEAD)"
 	Version string = "v0.0.6"
 
 	CranelibVersion string = cranelibversion.Version
+	BuildCommit     string = ""
 
 	KustomizeVersion string = readKustomizeVersion()
 )
