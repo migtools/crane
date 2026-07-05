9c4897fdf089cb1551db30be5edc851bc95e96c3
Nandini Chandra <nachandr@redhat.com>
2026-07-05 10:22:04 -0500

    :test_tube: Add input validation for --run-as flag (#599)
    * Add input validation for --run-as flag
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add input validation for --run-as flag
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- e2e-tests/config/config.go
+++ e2e-tests/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+	"log"
+)
+
 var (
 	K8sDeployBin          string
 	SourceContext         string
@@ -11,3 +16,20 @@ var (
 	InsecureSkipTLSVerify bool
 	RunAs                 string
 )
+
+// Validates the --run-as flag value and logs the active mode.
+// This should be called in BeforeSuite hooks in test suites.
+// Returns an error if the --run-as value is invalid.
+func ValidateAndLogRunAsFlag() error {
+	// Validate --run-as flag value
+	if RunAs != "" && RunAs != "admin" {
+		return fmt.Errorf("invalid --run-as value: %q. Valid values: \"admin\" or empty string (for non-admin mode)", RunAs)
+	}
+
+	if RunAs == "admin" {
+		log.Printf("[E2E] Running tests with --run-as=admin (cluster-admin credentials)")
+	} else {
+		log.Printf("[E2E] Running tests in non-admin mode")
+	}
+	return nil
+}
