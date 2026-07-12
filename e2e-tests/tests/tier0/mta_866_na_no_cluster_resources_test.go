2f16a60ef40d5232f9f16503c3864a83b48cf9f1
Ran Wurmbrand <rwurmbra@redhat.com>
2026-07-12 14:35:02 +0300

    Add tier0/tier1 RBAC e2e tests (MTA-856, MTA-866, MTA-867)
    Signed-off-by: Ran Wurmbrand <rwurmbra@redhat.com>

---
+++ e2e-tests/tests/tier0/mta_866_na_no_cluster_resources_test.go
@@ -0,0 +1,92 @@
+package e2e
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/konveyor/crane/e2e-tests/config"
+	. "github.com/konveyor/crane/e2e-tests/framework"
+	"github.com/konveyor/crane/e2e-tests/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Namespace-admin cluster-level migration", func() {
+	It("[NA-4] Should migrate namespace-only workload as namespace-admin without split apply", Label("tier0"), func() {
+		appName := "simple-nginx-nopv"
+		namespace := "simple-nginx-nopv"
+		serviceName := "my-" + appName
+		scenario := NewMigrationScenario(
+			appName,
+			namespace,
+			config.K8sDeployBin,
+			config.CraneBin,
+			config.SourceContext,
+			config.TargetContext,
+		)
+		if scenario.KubectlSrcNonAdmin.Context == "" {
+			Skip("source-nonadmin-context is required for non-admin role migration test")
+		}
+		if scenario.KubectlTgtNonAdmin.Context == "" {
+			Skip("target-nonadmin-context is required for non-admin role migration test")
+		}
+		srcAppNonAdmin := scenario.SrcAppNonAdmin
+		tgtAppNonAdmin := scenario.TgtAppNonAdmin
+
+		srcAppNonAdmin.ExtraVars = map[string]any{
+			"non_admin_user": "true",
+		}
+		tgtAppNonAdmin.ExtraVars = map[string]any{
+			"non_admin_user": "true",
+		}
+
+		runner := scenario.CraneNonAdmin
+		kubectlTgt := scenario.KubectlTgt
+		paths, err := NewScenarioPaths("crane-*")
+		Expect(err).NotTo(HaveOccurred())
+
+		exportOpts := ExportOptions{Namespace: srcAppNonAdmin.Namespace, ExportDir: paths.ExportDir}
+		transformOpts := TransformOptions{ExportDir: paths.ExportDir, TransformDir: paths.TransformDir}
+		applyOpts := ApplyOptions{ExportDir: paths.ExportDir, TransformDir: paths.TransformDir,
+			OutputDir: paths.OutputDir}
+
+		By("Granting namespace-admin permissions to non-admin user on source and target")
+		kubectlSrcNonAdmin, kubectlTgtNonAdmin, rbacCleanup, err := SetupNamespaceAdminUsersForScenario(scenario, namespace)
+		Expect(err).NotTo(HaveOccurred())
+
+		DeferCleanup(rbacCleanup)
+		DeferCleanup(func() {
+			rbacCleanup()
+
+			if err := CleanupScenario(paths.TempDir, srcAppNonAdmin, tgtAppNonAdmin); err != nil {
+				log.Printf("Scenario cleanup: %v", err)
+			}
+
+		})
+
+		By("Deploying namespace-only app as namespace-admin on source cluster")
+		err = PrepareSourceApp(srcAppNonAdmin, kubectlSrcNonAdmin)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Waiting for source pods and endpoints to drain")
+		WaitForSourceQuiesce(kubectlSrcNonAdmin, namespace, "app="+appName, serviceName)
+
+		By("Running crane export, transform, apply as namespace-admin")
+		Expect(RunCranePipelineWithChecks(runner, exportOpts, transformOpts, applyOpts)).NotTo(HaveOccurred())
+
+		By("Verifying no cluster resources was migrated")
+		clusterDir := filepath.Join(paths.OutputDir, "resources", "_cluster")
+		_, _, err = utils.HasFilesRecursively(clusterDir)
+		//we dont expct orphan cr's to be migrated,so _cluster dir should not be created.
+		Expect(err).To(HaveOccurred())
+
+		By("Applying namespace resources to target as namespace-admin")
+		Expect(kubectlTgt.ApplyDir(filepath.Join(paths.OutputDir))).NotTo(HaveOccurred())
+
+		By("Scaling target deployment and validating app")
+		Expect(kubectlTgtNonAdmin.ScaleDeployment(namespace, appName, 1)).NotTo(HaveOccurred())
+		Eventually(tgtAppNonAdmin.Validate, "2m", "10s").Should(Succeed())
+
+	})
+
+})
