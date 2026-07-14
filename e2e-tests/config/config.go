691fe93363faeb1ef4c9993b139af6a4d6c8e014
Nandini Chandra <nachandr@redhat.com>
2026-07-14 09:10:33 -0500

    :test_tube: Add input validation for --run-as flag (#649)
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- e2e-tests/config/config.go
+++ e2e-tests/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+
+	"github.com/onsi/ginkgo/v2"
+)
+
 var (
 	K8sDeployBin          string
 	SourceContext         string
@@ -11,3 +17,16 @@ var (
 	InsecureSkipTLSVerify bool
 	RunAs                 string
 )
+
+// ValidateAndLogRunAsFlag validates the --run-as flag and logs the mode
+func ValidateAndLogRunAsFlag() {
+	if RunAs != "" && RunAs != "admin" {
+		log.Fatalf("Invalid --run-as value: %q. Valid values: \"admin\" or empty string (for non-admin mode)", RunAs)
+	}
+
+	if RunAs == "admin" {
+		ginkgo.GinkgoWriter.Printf("Running e2e suite in admin mode\n")
+	} else {
+		ginkgo.GinkgoWriter.Printf("Running e2e suite in non-admin mode\n")
+	}
+}
