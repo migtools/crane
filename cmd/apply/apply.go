f55950e01b8a554fb6016b3029cc6d004863c981
Tamar-Dinavetsky <tdinavet@redhat.com>
2026-09-14 17:41:07 +0300

    Validate audit logger initialization in all Complete() methods

--- cmd/apply/apply.go
+++ cmd/apply/apply.go
@@ -109,7 +109,7 @@ If no stages specified, all discovered stages are applied.`,
 		PreRun: func(cmd *cobra.Command, args []string) {
 			viper.BindPFlags(cmd.Flags())
 			viper.Unmarshal(&o.Flags)
-			viper.Unmarshal(o.cobraGlobalFlags)
+			viper.Unmarshal(&o.globalFlags)
 		},
 	}
 
